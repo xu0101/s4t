55b36f1a0a451aed6199323ac057212d41291597
bxu3 <bxu3@cisco.com>
2016-07-14 15:50:57 +0800

    Update script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-
-xcodebuild -project Demo/Demo.xcodeproj -destination "platform=iOS Simulator,name=iPhone 6"
+xcodebuild [DEFAULT_OPTIONS] CODE_SIGNING_REQUIRED=NO
+xcodebuild -project Demo/Demo.xcodeproj -destination "platform=iOS Simulator,name=iPhone 6" CODE_SIGNING_REQUIRED=NO
