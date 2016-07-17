bc3f6c2213af04b2c80e5a38d880473485f37dc8
Bin Xu <bxu3@cisco.com>
2016-07-17 12:59:26 +0800

    Add podfile

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        print(Alamofire.AlamofireVersionNumber)
         return true
     }
 
