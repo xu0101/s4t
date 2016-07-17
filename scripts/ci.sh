8d2fbfcf60d85089cf03c3278c4b52498d888f78
Bin Xu <bxu3@cisco.com>
2016-07-17 13:00:50 +0800

    Update script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 set -e
-xcodebuild -sdk iphonesimulator -project Demo.xcodeproj -destination "platform=iOS Simulator,name=iPhone 6" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild -sdk iphonesimulator -workspace Demo.xcworkspace -destination "platform=iOS Simulator,name=iPhone 6" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
